0ab182bb33b9ce673ed2bf858c0bbedbf8c00f83
Chirag <140259132+chiraglaheru@users.noreply.github.com>
2025-10-31 14:41:28 +0530

    Implement number guessing game

---
+++ game.py
@@ -0,0 +1,30 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+secret_number = random.randint(1, 100)
+
+if level == 'easy':
+    max_tries = 10
+else:
+    max_tries = 5  
+tries_used = 0
+
+while tries_used < max_tries:
+    print(f"You have {max_tries - tries_used} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))  
+    
+    if guess == secret_number:
+        print("Congratulations! You guessed it!")
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+    
+    tries_used += 1
+
+if tries_used == max_tries and guess != secret_number:
+    print(f"Game over! The number was {secret_number}")
